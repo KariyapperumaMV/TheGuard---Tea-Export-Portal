3b438283fcbb261afe55c5f849c1ecf3284c7473
Hasitha <hasithapeiris.me@gmail.com>
2024-12-07 23:03:54 +0530

    feat: initialize express app

--- node-server/server.js
+++ node-server/server.js
@@ -1,1 +1,24 @@
-console.log("Hello world!");
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+// Load environment variables
+dotenv.config();
+
+// Initialize express app
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// Root Route
+app.get("/", (req, res) => {
+  res.send("Welcome to the Express Starter API!");
+});
+
+// Start the Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
