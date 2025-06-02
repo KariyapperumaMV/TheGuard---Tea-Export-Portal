e6c2e8c1cc15fc2a7762ab57d47e60f89bb0f5b5
Hasitha <hasithapeiris.me@gmail.com>
2025-06-02 13:43:39 +0530

    removed initial files

--- web-client/src/components/index.tsx
+++
@@ -1,6 +0,0 @@
-import MainCarousel from "./MainCarousel";
-import Navbar from "./Navbar";
-import Footer from "./Footer";
-import SubHeader from "./SubHeader";
-
-export { MainCarousel, Navbar, Footer, SubHeader };
--- web-client/src/pages/index.tsx
+++
@@ -1,6 +0,0 @@
-import Home from "./Home";
-import ForexPrediction from "./ForexPrediction";
-import Portal from "./Portal";
-import ForexPortal from "./ForexPortal";
-
-export { Home, ForexPrediction, Portal, ForexPortal };
